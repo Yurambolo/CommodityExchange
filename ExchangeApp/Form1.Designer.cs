732fdfe35c62cc6744ffced4bc71265f138e0df9
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2020-05-09 21:30:35 +0300

    Second commit

---
+++ ExchangeApp/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CommodityExchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.retailPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.wholePriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.minimalWholeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stockDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.exchangeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.unitDataGridViewTextBoxColumn,
+            this.retailPriceDataGridViewTextBoxColumn,
+            this.wholePriceDataGridViewTextBoxColumn,
+            this.minimalWholeDataGridViewTextBoxColumn,
+            this.stockDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.exchangeBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(744, 381);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // unitDataGridViewTextBoxColumn
+            // 
+            this.unitDataGridViewTextBoxColumn.DataPropertyName = "Unit";
+            this.unitDataGridViewTextBoxColumn.HeaderText = "Unit";
+            this.unitDataGridViewTextBoxColumn.Name = "unitDataGridViewTextBoxColumn";
+            // 
+            // retailPriceDataGridViewTextBoxColumn
+            // 
+            this.retailPriceDataGridViewTextBoxColumn.DataPropertyName = "RetailPrice";
+            this.retailPriceDataGridViewTextBoxColumn.HeaderText = "RetailPrice";
+            this.retailPriceDataGridViewTextBoxColumn.Name = "retailPriceDataGridViewTextBoxColumn";
+            // 
+            // wholePriceDataGridViewTextBoxColumn
+            // 
+            this.wholePriceDataGridViewTextBoxColumn.DataPropertyName = "WholePrice";
+            this.wholePriceDataGridViewTextBoxColumn.HeaderText = "WholePrice";
+            this.wholePriceDataGridViewTextBoxColumn.Name = "wholePriceDataGridViewTextBoxColumn";
+            // 
+            // minimalWholeDataGridViewTextBoxColumn
+            // 
+            this.minimalWholeDataGridViewTextBoxColumn.DataPropertyName = "MinimalWhole";
+            this.minimalWholeDataGridViewTextBoxColumn.HeaderText = "MinimalWhole";
+            this.minimalWholeDataGridViewTextBoxColumn.Name = "minimalWholeDataGridViewTextBoxColumn";
+            // 
+            // stockDataGridViewTextBoxColumn
+            // 
+            this.stockDataGridViewTextBoxColumn.DataPropertyName = "Stock";
+            this.stockDataGridViewTextBoxColumn.HeaderText = "Stock";
+            this.stockDataGridViewTextBoxColumn.Name = "stockDataGridViewTextBoxColumn";
+            // 
+            // exchangeBindingSource
+            // 
+            this.exchangeBindingSource.DataMember = "Products";
+            this.exchangeBindingSource.DataSource = typeof(CommodityExchange.Models.Exchange);
+            this.exchangeBindingSource.CurrentChanged += new System.EventHandler(this.exchangeBindingSource_CurrentChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exchangeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource exchangeBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn retailPriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn wholePriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn minimalWholeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stockDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExchangeApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CommodityExchange.Models;
+
+namespace CommodityExchange
+{
+    public partial class Form1 : Form
+    {
+
+        Exchange exchange = new Exchange();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exchangeBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //exchange.Load();
+            exchange.Users.Add(new User("Vasya","123","0123189","123@mail.ru"));
+            exchange.Users.Add(new User("Fedya", "321", "9172983", "321@mail.ru"));
+            exchange.Products.Add(new Product("1", "in", 400, 300, 12, 100, exchange.Users[0].Id));
+            exchange.Orders.Add(new Order(exchange.Users[0].Id, exchange.Users[1].Id, exchange.Products[0].Id, 2, "123"));
+            foreach (Product product in exchange.Products)
+            {
+                Console.WriteLine(product.Id);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.RetailPrice);
+                Console.WriteLine(product.WholePrice);
+                Console.WriteLine(product.Unit);
+                Console.WriteLine(product.MinimalWhole);
+            }
+            exchange.Save();
+            new DAL.Dao1(exchange).Save();
+        }
+    }
+}
